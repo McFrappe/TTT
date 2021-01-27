cd48383376c9fab8cfb8d6739a79a3b86ed4c2d9
McFrappe <McFrappe@users.noreply.github.com>
2021-01-27 22:12:30 +0000

    Apply formatting changes to .c and .h files (Astyle)

--- src/api.h
+++ src/api.h
@@ -4,15 +4,15 @@
 #include <curl/curl.h>
 
 enum pages {
-  HOME = 100,
-  NEWS = 101,
-  FOREIGN_NEWS = 104,
-  ECONOMY = 200,
-  SPORT = 300,
-  STOCK_MARKET = 330,
-  SPORT_SERVICE = 376,
-  TV = 600,
-  CONTENTS = 700
+    HOME = 100,
+    NEWS = 101,
+    FOREIGN_NEWS = 104,
+    ECONOMY = 200,
+    SPORT = 300,
+    STOCK_MARKET = 330,
+    SPORT_SERVICE = 376,
+    TV = 600,
+    CONTENTS = 700
 };
 
 typedef enum pages pages_t;
--- src/main.c
+++ src/main.c
@@ -4,15 +4,11 @@
 #include <stdlib.h>
 
 int main(void) {
-  api_intialize();
-  
-  page_t *home = api_get_page_range(HOME, FOREIGN_NEWS);
-  page_t *news = api_get_page(NEWS);
-  
-  free(home);
-  free(news);
-  
-  api_destroy();
-
-  return 0;
+    api_intialize();
+    page_t *home = api_get_page_range(HOME, FOREIGN_NEWS);
+    page_t *news = api_get_page(NEWS);
+    free(home);
+    free(news);
+    api_destroy();
+    return 0;
 }
