8b0998564a5e8219af735c00f04b446e2432ef04
mcfrappe <fahadrami.jamil.9983@student.uu.se>
2021-02-02 09:26:03 +0100

    UI: fixed so that colors reset after exiting application.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include "ui.h"
+#include <unistd.h>
 
 // TODO: Add CLI-arguments
 int main(void) {
     ui_initialize();
     ui_event_loop();
     ui_destroy();
+
+    execlp("reset", "reset", NULL);
     return 0;
 }
