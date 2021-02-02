cd3097b86f458e108795fde3adcd86c9d39a0039
Fredrik Engstrand <frewacom@gmail.com>
2021-02-02 10:17:18 +0100

    feat: call reset only if flag is set

--- src/main.c
+++ src/main.c
@@ -1,12 +1,19 @@
 #include "ui.h"
 #include <unistd.h>
 
-// TODO: Add CLI-arguments
-int main(void) {
+int main(int argc, char *argv[]) {
     ui_initialize();
     ui_event_loop();
     ui_destroy();
 
-    execlp("reset", "reset", NULL);
+    if (argc > 1) {
+        // Resetting is really slow, so we hide it behind '-r'
+        if (strcmp(argv[1], "-r") == 0) {
+            // Seems like this is one of the only ways to actually restore the terminal colors
+            // https://stackoverflow.com/questions/8686368/how-can-the-original-terminal-palette-be-acquired-preferably-using-ncurses-rout
+            execlp("reset", "reset", NULL);
+        }
+    }
+
     return 0;
 }
