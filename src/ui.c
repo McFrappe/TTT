8ca261b8fcf15e4805cc0d63358a3a4ef9728a4d
Fredrik Engstrand <frewacom@gmail.com>
2021-02-02 16:03:35 +0100

    feat: add page render buffer cache

--- src/ui.c
+++ src/ui.c
@@ -3,8 +3,28 @@
 // TODO: Add window buffer cache to prevent rerendering of pages when switching between VIEW_MAIN and VIEW_HELP
 // TODO: Add window where we will echo and take input
 static WINDOW *content_win;
+static int current_page_index = -1;
 static page_t *current_page = NULL;
 static page_collection_t *current_collection;
+static char page_cache[MAX_PAGE_COLLECTION_SIZE][PAGE_LINES * PAGE_COLS];
+
+static void save_view_buffer() {
+    assert(current_page_index >= 0);
+    assert(current_page_index < MAX_PAGE_COLLECTION_SIZE);
+    mvwinstr(content_win, PAGE_LINES, PAGE_COLS, page_cache[current_page_index]);
+}
+
+static void restore_page_buffer(int index) {
+    assert(index >= 0);
+    assert(index < MAX_PAGE_COLLECTION_SIZE);
+
+    if (page_cache[index][0] == '\0') {
+        return;
+    }
+
+    mvwaddstr(content_win, PAGE_LINES, PAGE_COLS, page_cache[index]);
+    page_cache[index][0] = '\0';
+}
 
 static void set_page(uint16_t page) {
     error_reset();
@@ -18,7 +38,8 @@ static void set_page(uint16_t page) {
     current_collection->pages[0] = page_create_empty();
     current_collection->pages[0]->id = 100;
     current_collection->pages[0]->title = strdup("Test page title");
-    // TODO: set the current page accordingly
+    // TODO: set the current page and current_page_index accordingly
+    current_page_index = 0;
     current_page = current_collection->pages[0];
     draw(content_win, VIEW_MAIN, current_page);
 }
@@ -40,6 +61,8 @@ static void resize_handler(int sig) {
 void ui_initialize() {
     setlocale(LC_ALL, "");
     initscr();
+    noecho();
+    nodelay(stdscr, TRUE);
     api_initialize();
     colors_initialize();
     content_win = newwin(
@@ -49,8 +72,12 @@ void ui_initialize() {
         (COLS / 2) - (PAGE_COLS / 2)
     );
     signal(SIGWINCH, resize_handler);
-    noecho();
-    nodelay(stdscr, TRUE);
+
+    // By checking the first char of each page buffer cache we can detect empty buffers
+    for (int i = 0; i < MAX_PAGE_COLLECTION_SIZE; i++) {
+        page_cache[i][0] = '\0';
+    }
+
     refresh();
     set_page(TTT_PAGE_HOME);
 }
